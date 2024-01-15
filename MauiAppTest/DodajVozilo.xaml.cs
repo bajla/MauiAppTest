b7329deea84d211502f99c6feaf8634bba5d2ef6
VegasCool <aney.vaishnav5@gmail.com>
2024-01-15 16:03:35 +0100

    fixes

--- MauiAppTest/DodajVozilo.xaml.cs
+++ MauiAppTest/DodajVozilo.xaml.cs
@@ -3,13 +3,114 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vozila;
 
 namespace MauiAppTest;
 
 public partial class DodajVozilo : ContentPage
 {
-    public DodajVozilo()
+    private HomePage _homePage;
+    public DodajVozilo(HomePage homePage)
     {
         InitializeComponent();
+        _homePage = homePage;
+
+
+    }
+
+    private void Nazaj_OnClicked(object? sender, EventArgs e)
+    {
+        Navigation.PopModalAsync();
+    }
+
+    private void IsCheckBoxChanged(object? sender, EventArgs e)
+    {
+
+        List<CheckBox> list = new List<CheckBox>() { Avto, Kolo, Coln, Jahta, Vodno, Kopensko };
+        CheckBox? check = sender as CheckBox;
+
+        foreach (var i in list)
+        {
+            if(i == check) continue;
+            i.IsChecked = false;
+        }
+
+        if (Avto.IsChecked)
+        {
+            Grid_Kolo.IsVisible = false;
+            Grid_Avto.IsVisible = true;
+            Grid_Coln.IsVisible = false;
+            Grid_Jahta.IsVisible = false;
+            Grid_Kopensko.IsVisible = false;
+            Grid_Vodno.IsVisible = false;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else if (Kolo.IsChecked)
+        {
+            Grid_Kolo.IsVisible = true;
+            Grid_Avto.IsVisible = false;
+            Grid_Coln.IsVisible = false;
+            Grid_Jahta.IsVisible = false;
+            Grid_Kopensko.IsVisible = false;
+            Grid_Vodno.IsVisible = false;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else if (Coln.IsChecked)
+        {
+            Grid_Coln.IsVisible = true;
+            Grid_Avto.IsVisible = false;
+            Grid_Kolo.IsVisible = false;
+            Grid_Jahta.IsVisible = false;
+            Grid_Kopensko.IsVisible = false;
+            Grid_Vodno.IsVisible = false;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else if (Jahta.IsChecked)
+        {
+            Grid_Coln.IsVisible = false;
+            Grid_Avto.IsVisible = false;
+            Grid_Kolo.IsVisible = false;
+            Grid_Jahta.IsVisible = true;
+            Grid_Kopensko.IsVisible = false;
+            Grid_Vodno.IsVisible = false;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else if(Kopensko.IsChecked)
+        {
+            Grid_Coln.IsVisible = false;
+            Grid_Avto.IsVisible = false;
+            Grid_Kolo.IsVisible = false;
+            Grid_Jahta.IsVisible = false;
+            Grid_Kopensko.IsVisible = true;
+            Grid_Vodno.IsVisible = false;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else if(Vodno.IsChecked)
+        {
+            Grid_Coln.IsVisible = false;
+            Grid_Avto.IsVisible = false;
+            Grid_Kolo.IsVisible = false;
+            Grid_Jahta.IsVisible = false;
+            Grid_Kopensko.IsVisible = false;
+            Grid_Vodno.IsVisible = true;
+            Btn_Dodaj.IsEnabled = true;
+        }
+        else
+        {
+            Btn_Dodaj.IsEnabled = false;
+        }
+        
+    }
+
+    private void Btn_Dodaj_OnClicked(object? sender, EventArgs e)
+    {
+        if (Avto.IsChecked)
+        {
+            Avto avto = new Avto(ImeVozila.Text, double.Parse(CenaVozila.Text),int.Parse(StSedezov.Text),int.Parse(LetnikVozila.Text),int.Parse(StLastnikov.Text),0,int.Parse(MaxHitrost.Text),4,int.Parse(Avto_VelMotorja.Text), int.Parse(Avto_StCilindrov.Text), int.Parse(Avto_MocMotorja.Text), int.Parse(Avto_Poraba.Text), Avto_VrstaGoriva.Text);
+            HomePage.VsaVozila.Add(avto);
+            _homePage.RefreshList();
+            Navigation.PopModalAsync();
+
+        }
     }
 }
--- MauiAppTest/HomePage.xaml.cs
+++ MauiAppTest/HomePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vozila;
 
 namespace MauiAppTest;
 
@@ -10,13 +11,14 @@ public partial class HomePage : ContentPage
 {
     private bool _osebnaInvSelected = true;
     private bool _najemSelected = true;
-    
+
+    public static List<Vozilo> VsaVozila = new List<Vozilo>();
     
     public HomePage()
     {
         InitializeComponent();
         NavigationPage.SetHasBackButton(this, false);
-
+        
         Grid grid = new Grid
         {
             RowDefinitions =
@@ -43,18 +45,20 @@ public partial class HomePage : ContentPage
         }, 1,0);
         grid.Add(new Label
         {
-            Text = "Stanje vozila"
+            Text = "Vrsta vozila"
         }, 2,0);
         grid.Add(new Label
         {
             Text = "Letnik vozila"
         }, 3,0);
-        grid.Add(new Button()
+        grid.Add(new Label()
         {
             Text = "Spremeni"
         }, 4,0);
 
         Vozila_stack.Add(grid);
+
+        
         
     }
 
@@ -89,6 +93,52 @@ public partial class HomePage : ContentPage
 
     private void Dodaj_btn_OnClicked(object? sender, EventArgs e)
     {
-        
+        Navigation.PushModalAsync(new DodajVozilo(this));
     }
+
+    public void RefreshList()
+    {
+        foreach (var i in VsaVozila)
+        {
+            Grid grid1 = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition()
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition(),
+                    new ColumnDefinition(),
+                    new ColumnDefinition(),
+                    new ColumnDefinition(),
+                    new ColumnDefinition()
+                }
+            };
+        
+            grid1.Add(new Label
+            {
+                Text = i.Ime
+            }, 0,0);
+            grid1.Add(new Label
+            {
+                Text = i.Cena.ToString()
+            }, 1,0);
+            grid1.Add(new Label
+            {
+                Text = i.GetType().ToString()
+            }, 2,0);
+            grid1.Add(new Label
+            {
+                Text = i.LetnikVozila.ToString()
+            }, 3,0);
+            grid1.Add(new Button()
+            {
+                Text = "Spremeni"
+            }, 4,0);
+
+            Vozila_stack.Add(grid1);
+        }
+    }
+    
 }
